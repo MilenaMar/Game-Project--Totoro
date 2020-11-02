4280cc5e5783e7e42354ea4d9f50d471f78a7ecf
Ana Milena Martinez Caceres <67890212+MilenaMar@users.noreply.github.com>
2020-11-02 12:06:22 +0000

    Game Status  implemented

--- Game.js
+++ Game.js
@@ -19,18 +19,39 @@ class Game {
         this.food.splice(index, 1);
       }
    });
-
+  
    if (frameCount % 60 === 0) {
     this.dust.push(new Dust());
   }
 
-  this.dust.forEach((dustOb, index) => {
+  this.dust.forEach((dustOb, index) => { 
     dustOb.draw();
     if (dustOb.y + dustOb.width <= 0) {
       this.dust.splice(index, 1);
     }
+
+    if (this.colisionCheck(dustOb, this.player)) {
+      noLoop();
+    }
  });
+
     }
 
+    colisionCheck(dustOb, player) {
+      if ((player.x-30) + player.width < dustOb.x) {
+        return false;
+      }
+    
+      if (dustOb.x + dustOb.width < (player.x+30) ){
+        return false;
+      }
+      if ((player.y+20) > dustOb.y + dustOb.height) {
+        return false;
+      }
+      if (dustOb.y > player.y + player.height) {
+        return false;
+      }
+      return true;
+    }
 
   }
--- main.js
+++ main.js
@@ -6,17 +6,21 @@ function preload() {
    
   }
   const game = new Game();
+  const scoreBox = document.querySelector(".score span");
+  let gameStatus = false;
 
   function setup() {
     createCanvas(WIDTH,HEIGHT);
-
   }
 
   function draw() {
     background("cyan");
     image(backImg, 0, 0, width, height);
+    if (gameStatus === true){
     game.draw();
-  }
+    }
+}
+
 
   function keyPressed() {
      if (keyCode === 37) {
@@ -24,4 +28,13 @@ function preload() {
     } else if (keyCode === 39) {
       game.player.moveRight();
     }
+
+    // Press Space Bar to start the game // Game status change to True
+      if (keyCode === 32){
+      gameStatus = true;
+      document.querySelector('div.start-game').style.visibility = "hidden";
+      scoreBox.style.visibility = 'visible';
+      } 
   }
+
+
